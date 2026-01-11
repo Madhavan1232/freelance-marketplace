b0739c43998bb55c706add438ab0f37322d7d608
Madhavan1232 <madhavanvairavan3@gmail.com>
2026-01-11 14:51:25 +0530

    final

--- src/App.js
+++ src/App.js
@@ -1,28 +1,29 @@
-import {Routes, Route } from 'react-router-dom';
-import Navbar from './NavBar/Navbar.js';
-import Home from './Home/Home.jsx';
-import Signup from './Signup/Signup.jsx';
-import Login from './Login/Login.jsx';
-import PasswordPage from './PasswordPage/PasswordPage.jsx';
-import ProjectPage from './Projects/ProjectPage.jsx';
-import FreelancerProfile from './FreelancerProfile/FreelancerProfile.jsx';
-import About from './About/About.jsx';
-import Talents from './Talents/Talents.jsx';
+import { Routes, Route } from 'react-router-dom';
+import Navbar from './NavBar/Navbar';
+import Home from './Home/Home';
+import Signup from './Signup/Signup';
+import Login from './Login/Login';
+import PasswordPage from './PasswordPage/PasswordPage';
+import ProjectPage from './Projects/ProjectPage';
+import FreelancerProfile from './FreelancerProfile/FreelancerProfile';
+import About from './About/About';
+import Talents from './Talents/Talents';
+
 function App() {
   return (
-      <div style={{ minHeight: '100vh', display: 'flex', flexDirection: 'column' }}>
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/signup" element={<Login />} />
-          <Route path='/login' element={<Signup />} />
-          <Route path='/password' element={<PasswordPage />} />
-          <Route path='/projects' element={<ProjectPage />} />
-          <Route path='/freelancer-profile' element={<FreelancerProfile />} />
-          <Route path='/about' element={<About />} />
-          <Route path='/talents' element={<Talents />} />
-        </Routes>
-      </div>
+    <div style={{ minHeight: '100vh', display: 'flex', flexDirection: 'column' }}>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/password" element={<PasswordPage />} />
+        <Route path="/projects" element={<ProjectPage />} />
+        <Route path="/freelancer-profile" element={<FreelancerProfile />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/talents" element={<Talents />} />
+      </Routes>
+    </div>
   );
 }
 
